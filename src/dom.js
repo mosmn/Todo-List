e38380071484a0379487a2db6d75f76be110d5b0
mosmn <ithinkisee8@gmail.com>
2023-06-08 05:39:57 +0800

    created the main containers layout and basic add todo functionality

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,6 @@
 import './style.css';
 import {todoList, createNewTodo, addTodo} from './todo.js';
 
-// a factory function that takes in a todo object and returns a dom elements
-
 const createElement = (tagName, className, textContent) => {
     const element = document.createElement(tagName);
     element.classList.add(className);
@@ -22,5 +20,39 @@ const taskFactory = (todo) => {
     return task;
 }
 
-const mainContainer = createElement('div', 'main-container', '');
-document.body.appendChild(mainContainer);
+const initialPageLoad = () => {
+    const mainContainer = createElement('div', 'main-container', '');
+    mainContainer.innerHTML = `
+        <div class="header-bar">
+            <img src="https://img.icons8.com/ios/50/000000/todo-list-filled.png"/>
+            <h1>Todo List</h1>
+        </div>
+        <div class="sidebar">
+            <div class="sidebar-title">Projects</div>
+            <div class="sidebar-projects">
+                <div class="sidebar-project">Project 1</div>
+                <div class="sidebar-project">Project 2</div>
+                <div class="sidebar-project">Project 3</div>
+            </div>
+        </div>
+        <div class="tasks">
+            <div class="task-form">
+                <input class="task-title" type="text" placeholder="Title">
+                <input class="task-description" type="text" placeholder="Description">
+                <input class="task-due-date" type="date">
+                <select class="task-priority">
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+                <input class="task-submit" type="submit" value="Add Task">
+            </div>
+            <div class="task-list"></div>
+        </div>
+    `   
+    document.body.appendChild(mainContainer);
+}
+
+document.addEventListener('DOMContentLoaded', initialPageLoad);
+
+
